706d959fe9d0de89dafed9db71e877fe7c2f0d14
abhishek15256 <75597125+abhishek15256@users.noreply.github.com>
2021-05-01 18:52:33 +0530

    commit

--- ajecom/settings.py
+++ ajecom/settings.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 import django_heroku
 import dj_database_url
-from decouple import config
+
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(
@@ -137,9 +137,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 STATIC_URL = '/static/'
-STATICFILE_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
