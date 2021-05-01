5eb35b381beb36f03c8b555354bb94543166c0f7
abhishek15256 <75597125+abhishek15256@users.noreply.github.com>
2021-05-01 18:57:34 +0530

    Update settings.py

--- ajecom/settings.py
+++ ajecom/settings.py
@@ -139,10 +139,8 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 STATIC_URL = '/static/'
-<<<<<<< HEAD
-=======
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
->>>>>>> 13faf66d342ad39ab74f8a0ada30cc24bc988f3f
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
