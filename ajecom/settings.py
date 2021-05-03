5de25498abd31f8f34d76e0fac00ed412a8be043
abhishek15256 <75597125+abhishek15256@users.noreply.github.com>
2021-05-03 14:03:40 +0530

    Update settings.py

--- ajecom/settings.py
+++ ajecom/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^1#s9kl36%s439irwd4gou6&-1n^=-mqtt1a)7bzouykp%(tb)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["ajeco.herokuapp.com"]
 
 
 # Application definition
